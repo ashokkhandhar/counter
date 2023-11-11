55688edc0d3946c45cfc67b9213b4dd5c25bc03e
ashok khandhar <ashokkhandhar18@gmail.com>
2023-11-11 23:31:11 +0530

    used keydown in place of keyup

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ const btns = document.querySelectorAll(".btn");
 var count = 0;
 
 btns.forEach((btn) => {
-    btn.addEventListener('click', function(e){
+    btn.addEventListener('mousedown', function(e){
         const styles = e.currentTarget.classList;
-
         if (styles.contains("decrease")) count--;
         else if (styles.contains("increase")) count++;
         else count = 0;
-        colorStyling(count);
         value.textContent = count;
+        colorStyling(count);
     });
 });
+
 function colorStyling(count){
     if (count > 0) value.style.color = "green";
-    if (count < 0) value.style.color = "red";
-    if (count === 0) value.style.color = "black";
+    else if (count < 0) value.style.color = "red";
+    else value.style.color = "#fff";
 }
-document.addEventListener('keyup', (event) => {
+
+document.addEventListener('keydown', (event) => {
     if (event.key === '+'){
         value.textContent = ++count;
         colorStyling(count);
     }
-}, false);
-document.addEventListener('keyup', (event) => {
+});
+document.addEventListener('keydown', (event) => {
     if (event.key === '-'){
         value.textContent = --count;
         colorStyling(count);
     }
- }, false);
+});
